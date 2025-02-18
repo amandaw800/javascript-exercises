39b58a62f28aee83bee9ac40f3784b75d3998b87
amandaw800 <amanda.v.wei@gmail.com>
2025-02-17 21:53:02 -0500

    sumAll.js is complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...remove) {
+    const ans = []
+
+    for(var i  = 0; i < arr.length; i++)
+    {
+        if(arr[i] != remove)
+        {
+            ans.push(arr[i]);
+        }
+    }
+    console.log(ans);
+    return ans;
 };
+
+
+
+
+
+
+
+
+
+removeFromArray([1,2,3,4], 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
