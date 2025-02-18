fe4af2e93087b0af60d15659831e110edc623f91
amandaw800 <amanda.v.wei@gmail.com>
2025-02-18 11:26:17 -0500

    finshed sumall

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,40 @@
-const sumAll = function() {
+const sumAll = function(num, num2) {
 
-};
+    var ans = 0;
+    if(num < 0 || num2 < 0)
+    {
+        return "ERROR";
+    }
+
+    if(Number.isInteger(num) == false || Number.isInteger(num2) == false)
+    {
+        return "ERROR";
+
+    }
+
+    if(num < num2)
+    {
+
+        for(var i = num; i <= num2; i++)
+        {
+            ans += i;
+        }
+    }
+    else if(num2 < num)
+        {
+    
+            for(var i = num2; i <= num; i++)
+            {
+                ans += i;
+            }
+        }
+        else {
+            return num;
+        }
+    return ans;
+
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
